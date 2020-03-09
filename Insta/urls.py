1c217c8fd2ec0eb4f16cbe34aa7d9c214d5c9662
lyulingxue <lyulingxue@hotmail.com>
2020-03-09 01:53:22 -0400

    first draft

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld
+from Insta.views import (HelloWorld, PostsView, PostDetailView, PostCreateView,
+                        PostUpdateView, PostDeleteView,EditProfile, ExploreView, SignUp, UserProfile, addComment,
+                         addLike, toggleFollow)
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld'),
+    path('helloworld/', HelloWorld.as_view(), name='helloworld'),
+    path('', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+    path('auth/signup', SignUp.as_view(), name='signup'),
+    path('user_profile/<int:pk>/', UserProfile.as_view(), name='profile'),
+    path('edit_profile/<int:pk>/', EditProfile.as_view(), name='edit_profile'),
+    path('togglefollow', toggleFollow, name='togglefollow'),
+    path('like', addLike, name='addLike'),
+    path('comment', addComment, name='addComment'),
+    path('explore', ExploreView.as_view(), name='explore'),
 ]

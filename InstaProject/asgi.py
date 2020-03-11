07d51cbed66ba29f1945f69a8e80e00b3307a353
lyulingxue <lyulingxue@hotmail.com>
2020-03-11 10:58:35 -0400

    Prep for deployment

--- InstaProject/asgi.py
+++ InstaProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for InstaJZ project.
+ASGI config for InstaProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InstaJZ.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InstaProject.settings')
 
 application = get_asgi_application()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn InstaProject.wsgi --log-file -
